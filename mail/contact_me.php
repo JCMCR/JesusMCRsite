e2f1f0022691af7b8055ccf6214ca284d3571819
James B <james.black@cantab.net>
2014-10-16 13:37:18 +0100

    contact form

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -1,10 +1,6 @@
 <?php
 // Check for empty fields
-if(empty($_POST['name'])  		||
-   empty($_POST['email']) 		||
-   empty($_POST['phone']) 		||
-   empty($_POST['message'])	||
-   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+if(empty($_POST['message'])
    {
 	echo "No arguments Provided!";
 	return false;
